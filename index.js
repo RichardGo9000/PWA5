ad4bf450d207e2c49198f485a0c0cedddbe4ac47
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-02-12 18:04:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// Web App Code
-const images = ['fox1', 'fox2', 'fox3', 'fox4'];
-const imgElem = document.querySelector('img');
+// // Web App Code
+// const images = ['fox1', 'fox2', 'fox3', 'fox4'];
+// const imgElem = document.querySelector('img');
 
-function randomValueFromArray(array) {
-  const randomNo = Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
+// function randomValueFromArray(array) {
+//   const randomNo = Math.floor(Math.random() * array.length);
+//   return array[randomNo];
+// }
 
-setInterval(() => {
-  const randomChoice = randomValueFromArray(images);
-  imgElem.src = `images/${randomChoice}.jpg`;
-}, 2000);
+// setInterval(() => {
+//   const randomChoice = randomValueFromArray(images);
+//   imgElem.src = `images/${randomChoice}.jpg`;
+// }, 2000);
 
 // Register service worker to control making site work offline
 
